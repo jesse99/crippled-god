03385b33f8034216d8bba09c5567b23d20605dda
Jesse Vorisek <jesse.vorisek@gmail.com>
2019-05-24 21:17:40 -0700

    Working on logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
 //#[macro_use]
 extern crate structopt;
 
+use slog::Drain;
+use std::fs::OpenOptions;
+use std::str::FromStr;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -12,13 +19,40 @@ struct Options {
 	#[structopt(long = "log-path", default_value = "crippled-god.log")]
 	log_path: String,
 
-	// #[structopt(long = "log-level", default_value = "42")]
-	// log_level: log::LogLevelFilter,
+	#[structopt(long = "log-level", default_value = "info")]
+	log_level: String,
+
 	#[structopt(long = "seed", default_value = "0")]
 	seed: usize,
 }
 
 fn main() {
 	let options = Options::from_args();
-	println!("log_path={} seed={}", options.log_path, options.seed);
+
+	let log_file = OpenOptions::new()
+		.create(true)
+		.write(true)
+		.truncate(true)
+		.open(options.log_path)
+		.unwrap();
+
+	let level = match slog::Level::from_str(&options.log_level) {
+		Ok(l) => l,
+		Err(_) => {
+			eprintln!("--log-level should be critical, error, warning, info, debug, or trace");
+			std::process::exit(1);
+		}
+	};
+	let decorator = slog_term::PlainDecorator::new(log_file);
+	let drain = slog_term::FullFormat::new(decorator).build().fuse();
+	let drain = slog_async::Async::new(drain).build().fuse();
+	let drain = slog::LevelFilter::new(drain, level).fuse();
+
+	let root_logger = slog::Logger::root(drain, o!());
+
+	let app_logger = root_logger.new(o!("version" => env!("CARGO_PKG_VERSION")));
+	info!(app_logger, "started up"; "seed" => options.seed);
+
+	let monster_logger = root_logger.new(o!("name" => "Ay", "level" => "Dungeon #5"));
+	debug!(monster_logger, "woke up"; "reason" => "heard player");
 }
